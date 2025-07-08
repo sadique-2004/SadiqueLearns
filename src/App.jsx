bee6e5544f587346bea172ea337e811feda8bf98
sadique-2004 <mds044234@gmail.com>
2025-07-08 16:40:55 +0530

    Apply conditional rendering based on the loading data from Appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice.js";
+import { Header, Footer } from "./components/index.js";
 
-import './App.css'
+import authService from "./appwrite/auth.js";
+// import { Outlet } from "react-router-dom";
 
 function App() {
+  const [loading, setLoaing] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-   <h1>One code with sadique</h1>
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => { setLoaing(false) });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen bg-amber-600 flex flex-wrap content-between">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet/>   ToDo */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div className="min-h-screen bg-red-600 text-center text-2xl content-center">.....Loading</div>
   )
 }
 
-export default App
+export default App;
