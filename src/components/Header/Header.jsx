1cba6d9081f27172acacf15fc87b53daa72a6932
sadique-2004 <mds044234@gmail.com>
2025-07-09 23:43:05 +0530

    Update Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import React from "react";
+import { Container, LogoutBtn, logo } from "../index";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const authStatus = useSelector((state) => state.auth.status);
 
-export default Header
+  const navigate = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+  return (
+    <header className="py-3 shadow bg-gray-500">
+      <Container>
+        <nav>
+          <Link to="/">
+            <img src={logo} alt="Sadique Learns" className="h-24" />
+          </Link>
+        </nav>
+
+        <ul className="flex ml-auto">
+          {navItems.map((item) =>
+            item.active ? (
+              <li key={item.name}>
+                <button
+                  onClick={() => {
+                    navigate(item.slug);
+                  }}
+                  className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                >
+                  {item.name}
+                </button>
+              </li>
+            ) : null
+          )}
+
+          {authStatus && (
+            <li>
+              <LogoutBtn />
+            </li>
+          )}
+        </ul>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
